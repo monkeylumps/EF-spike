218bfd5ed065c59f3786ca293505a3e8c1078eef
Unknown <daniel111999@gmail.com>
2018-04-02 13:47:57 +0100

    Created repo and started work on spike

---
+++ EF spike/Scheme/Controllers/SchemeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using spike.Scheme.Models;
+
+namespace EF_spike.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Scheme")]
+    public class SchemeController : Controller
+    {
+        [HttpGet]
+        public Scheme Get()
+        {
+            return new Scheme();
+        }
+    }
+}
