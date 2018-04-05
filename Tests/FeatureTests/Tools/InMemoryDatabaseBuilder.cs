c9476bd15c1aa7fdcfaaad9390e001ccb73e617e
Unknown <daniel111999@gmail.com>
2018-04-05 14:35:14 +0100

    moved to sqlite in mem database and fixed errors around the tests.

--- Tests/FeatureTests/Tools/InMemoryDatabaseBuilder.cs
+++ Tests/FeatureTests/Tools/InMemoryDatabaseBuilder.cs
@@ -10,5 +10,74 @@ namespace FeatureTests.Tools
             context.Add(entity);
             context.SaveChanges();
         }
+
+        public void AddReferanceData(RegistryContext registryContext, int psr)
+        {
+            AddEntityToDb(new TblMembershipBenefitType
+            {
+                MembershipBenefitTypeReference = 1,
+                MembershipBenefitTypeDescription = "test",
+                SortOrder = 1
+            }, registryContext);
+
+            AddEntityToDb(new TblMembershipType
+            {
+                MembershipTypeReference = 1,
+                MembershipTypeDescription = "test",
+                SortOrder = 1
+            }, registryContext);
+
+            AddEntityToDb(new TblEventTypeGroup()
+            {
+                EventTypeGroupReference = 1,
+                SortOrder = 1,
+                EventTypeGroupDescription = "test"
+            }, registryContext);
+
+            AddEntityToDb(new TblEventType
+            {
+                EventTypeDescription = "test",
+                EventTypeGroupReference = 1,
+                SortOrder = 1
+            }, registryContext);
+
+            AddEntityToDb(new TblEventType
+            {
+                EventType = 8,
+                EventTypeDescription = "test2",
+                EventTypeGroupReference = 1,
+                SortOrder = 1
+            }, registryContext);
+
+            AddEntityToDb(new TblEventSource
+            {
+                EventSourceDescription = "test",
+                EventSourceReference = 1,
+                SortOrder = 1
+            }, registryContext);
+
+            AddEntityToDb(new TblMembershipAverageAgeBasisType
+            {
+                MembershipAverageAgeBasisDescription = "test",
+                MembershipAverageAgeBasis = 1
+            }, registryContext);
+
+            AddEntityToDb(new TblMembershipAverageAgeBasisType
+            {
+                MembershipAverageAgeBasisDescription = "test3",
+                MembershipAverageAgeBasis = 3,
+            }, registryContext);
+
+            AddEntityToDb(new TblEvent
+            {
+                EventType = 1,
+                Psrnumber = psr,
+                SectionNumber = 0,
+                UserId = "test",
+                CreateDateTime = DateTime.Now,
+                NotificationDate = DateTime.Now,
+                EventSourceReference = 1
+            }, registryContext);
+        }
     }
 }
