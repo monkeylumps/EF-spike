3be89d6234da5eb46bf6f9622319011f9f58df11
Unknown <daniel111999@gmail.com>
2018-04-26 16:21:35 +0100

    changed EF retry policy to polly policy for useages where we dont use EF

--- WebApi/EF Spike/Shared/Polly/Handler/PollyDecorator.cs
+++ WebApi/EF Spike/Shared/Polly/Handler/PollyDecorator.cs
@@ -1,8 +1,9 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using EF_Spike.DatabaseContext;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
+using Polly;
 
 namespace EF_Spike.Shared.Polly.Handler
 {
@@ -17,18 +18,25 @@ namespace EF_Spike.Shared.Polly.Handler
 
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            var strategy = context.Database.CreateExecutionStrategy();
+            var policy = Policy.Handle<Exception>()
+                .WaitAndRetryAsync(6, // We can also do this with WaitAndRetryForever... but chose WaitAndRetry this time.
+                    attempt => TimeSpan.FromSeconds(0.1 * Math.Pow(2, attempt)), // Back off!  2, 4, 8, 16 etc times 1/4-second
+                    (exception, calculatedWaitDuration) =>  // Capture some info for logging!
+                    {});
 
-            return await strategy.ExecuteAsync(async () =>
+            var thing = await  policy.ExecuteAndCaptureAsync(async () =>
             {
                 using (var transaction = context.Database.BeginTransaction())
                 {
                     var result = await next();
+
                     transaction.Commit();
 
                     return result;
                 }
             });
+
+            return thing.Result;
         }
     }
 }
